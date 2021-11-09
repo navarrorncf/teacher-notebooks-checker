b236942484ee1f691e3036b27f4930583e0b1b68
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-11-09 09:45:58 -0300

    added basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    port: 9000,
+  },
+};
