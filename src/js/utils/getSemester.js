8330c3d385decf0f9d27788df2f203edec827932
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-11-11 08:44:35 -0300

    added module and function default exports

--- src/js/utils/getSemester.js
+++ src/js/utils/getSemester.js
@@ -1,2 +1,3 @@
-export const getSemester = (bimester) =>
-  `s${Math.ceil(parseInt(bimester) / 2)}`;
+const getSemester = (bimester) => `s${Math.ceil(parseInt(bimester) / 2)}`;
+
+export default getSemester;
---
+++ src/js/utils/index.js
@@ -0,0 +1,1 @@
+export { default as getSemester } from "./getSemester";
