c171e927693b8748201a726423912e2601b14e9b
Rafael Navarro C. Filho <rncf1112@gmail.com>
2021-11-11 09:47:37 -0300

    added style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         test: /\.txt$/,
         type: "asset/source",
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   devServer: {
